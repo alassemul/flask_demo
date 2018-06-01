185f48ef14ac379df1681267f417cce99e21efab
alass <sishuming6@163.com>
2018-06-01 15:17:28 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://alass:open0930@127.0.0.1:3306/fibersupport'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'alass'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(16))
+    password = db.Column(db.String(16))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(16), unique=True)
+
+
+class RegForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    password2 = PasswordField('确认密码', validators=[DataRequired(), EqualTo('password', message='密码不一致')])
+    submit = SubmitField('注册')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    submit = SubmitField('登陆')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if form.validate_on_submit():
+            return render_template("main.html")
+    return render_template("login.html", form=form)
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    form = RegForm()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        if form.validate_on_submit():
+            a = User()  # type: User
+            a.user_name = username
+            a.password = password
+            a.role_id = 2
+            db.session.add(a)
+            db.session.commit()
+            flash('注册成功')
+            return redirect('/')
+    else:
+            flash('参数有误')
+
+    return render_template("reg.html", form=form)
+
+
+if __name__ == '__main__':
+    app.run()
